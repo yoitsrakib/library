6ac676acd367146d9c29b7d649f7eb079376bdd0
yoitsrakib <yoitsrakibforya@gmail.com>
2022-05-08 23:49:44 +0600

    working

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
-let myLibrary = []; //Blank Library
+let myLibrary = [];
+
+function Book (Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
+}
+
+
+function pushToLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
+    myLibrary.push(book);
+    displayBooks();
+
+}
 
 
 
-//Constructor
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + read
+
+function displayBooks() {
+    const books = document.querySelector('.books');
+
+    const removeDivs = document.querySelectorAll('.cards');
+    for (let i=0; i<removeDivs.length; i++){
+        removeDivs[i].remove();
     }
- 
+  
+    //loop over library
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement('div');
+        card.classList.add('cards');
+        books.appendChild(card);
+        const removeButton = document.createElement('button');
+        removeButton.classList.add('removeButton')
+        removeButton.textContent = 'Remove';
+        card.appendChild(removeButton);
+      
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const paragraph = document.createElement('p');
+            paragraph.textContent=(`${key}: ${myLibrary[key]}`);
+            card.appendChild(paragraph);
+            
+        }
+    })
+
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-const harryPotter = new Book('Harry Potter', 'J.K Rowling', '295', 'not read yet');
+// pushToLibrary('gasfd', 'sdasd', 'sdsdas', 'sdsd');
 
 
+const submitButton = document.querySelector('.Submit');
 
-console.log(theHobbit.info());
+submitButton.addEventListener('click', () => {
+    let Title = document.getElementById('Title').value;
+    let Author = document.getElementById('Author').value;
+    let Pages = document.getElementById('Pages').value;
+    let Read = document.getElementById('Read').value;
 
-// Function to add Books to myLibrary
+    if ((Title== '') || (Author == '') || (Pages == '') || (Read == '')) {
+        return;
+    }
 
-function addToLibrary() {
-    myLibrary.push(theHobbit)
-    myLibrary.push(harryPotter)
-}
+    pushToLibrary(Title, Author, Pages, Read);
 
-addToLibrary();
+    document.getElementById('add-book').reset();
+  
+})
 
-console.log(myLibrary);
+
